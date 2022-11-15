c061b6300f285bc8be388f77d6d17036502af297
Oxygenium55 <114757285+Oxygenium55@users.noreply.github.com>
2022-11-15 17:25:17 +0600

    Task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,3 +3,16 @@
 1, -7, 567, 89, 223-> 3
 */
 
+Console.WriteLine("Введите числа: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(", "), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Количество чисел больше 0: {count}");
