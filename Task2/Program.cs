ef58dd44cb18bdc3a778abd481a6d2fcb6ad482a
Oxygenium55 <114757285+Oxygenium55@users.noreply.github.com>
2022-11-16 17:40:10 +0600

    Task2 done

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,27 @@
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
 
+double GetNumber(string msg)
+{
+    Console.Write(msg);
+    double userNumber = Convert.ToDouble(Console.ReadLine());
+    return userNumber;
+}
+
+double k1 = GetNumber("Введите число k1: ");
+double b1 = GetNumber("Введите число b1: ");
+double k2 = GetNumber("Введите число k2: ");
+double b2 = GetNumber("Введите число b2: ");
+
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+else
+{
+    var x = (b2 - b1) / (k1 - k2);
+    var y = k1 * x + b1;
+    x = Math.Round(x, 2);
+    y = Math.Round(y, 2);
+    Console.WriteLine($"Прямые пересекаются в точке ({x}; {y}).");
+}
